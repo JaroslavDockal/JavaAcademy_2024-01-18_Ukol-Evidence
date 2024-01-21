0d960a4cd0561f9bd44812a4fe7f3969cf8c1d7d
Jaroslav Dockal <jaroslav.dockal@outlook.com>
2024-01-21 02:42:34 -0800

    První verze

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Main {
+/*
+V metodě main svého projektu připrav proměnné pro uložení informací o prodejcích kvalitní biomrkve.
+Pokud to umíš, můžeš založit novou třídu a údaje o prodejcích zapsat jako atributy této třídy.
+
+O každém prodejci potřebujeme evidovat tyto údaje:
+
+Jméno a příjmení prodejce
+Datum narození prodejce.
+Počet dosud sjednaných smluv.
+Celkové množství prodané mrkve v tunách.
+Název města, kde prodejce sídlí.
+Registrační značka vozidla („SPZ“).
+Spotřeba firemního vozidla v litrech na 100 km.
+IP adresa verze 4 firemního počítače.
+Více o formátu IP adresy verze 4 viz například IP adresa verze 4
+
+Do proměnných ulož vhodné hodnoty (hodnoty si vymysli tak, aby dávaly smysl).
+
+Na základě zadaných údajů vypiš v metodě main na obrazovku průměrné množství prodané mrkve na jednu smlouvu.
+*/
+// Class Declaration
+
+import java.time.LocalDate;
+import java.net.InetAddress;
+
+public class Main {
+    public static void main(String[] args){
+        String divider = "\n--------------------------------------------------------------------------------------------";
+        int sumOfContract;
+        double sumOfSoldAmount;
+
+        Seller seller01
+                = new Seller("Tomáš Fuk", LocalDate.of(2002,07,01) , 5,
+                2.6, "Stará při Čopni", "2P4 3376", 8.3, "68.5.2.12" );
+
+        Seller seller02
+                = new Seller("František Skočdopole", LocalDate.of(1932,01,16) , 7,
+                3.1, "Malá Velká", "4S4 2132", 4.6, "13.37.55.63" );
+
+        Seller seller03
+                = new Seller("Kleofáš Zajíc", LocalDate.of(1984,04,01) , 2,
+                1.33, "Rychlá", "5U0 2736", 12.7, "138.241.4.31" );
+
+        System.out.println("Data ke dni: " + LocalDate.now() + "." + divider);
+        System.out.println(seller01.toString() + divider);
+        System.out.println(seller02.toString() + divider);
+        System.out.println(seller03.toString() + divider);
+
+        sumOfContract = seller01.noOfContracts + seller02.noOfContracts + seller03.noOfContracts;
+        sumOfSoldAmount = seller01.soldAmount + seller02.soldAmount + seller03.soldAmount;
+
+        System.out.println("Celkem dodáno " + sumOfSoldAmount + " tun zboží, navázáno na " + sumOfContract + " smluv.");
+        System.out.println("Průměrné množství zboží na 1 smlouvu je " + String.format("%.0f", 1000*sumOfSoldAmount/sumOfContract) + " kg.");
+    }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Seller {
+import java.time.LocalDate;
+//import java.text.DecimalFormat;
+import java.net.InetAddress;
+
+public class Seller {
+    String name;
+    LocalDate dateOfBirth;
+    int noOfContracts;
+    double soldAmount;
+    String city;
+    String spz;
+    double consumption;
+    String ip;
+
+    public Seller(String name, LocalDate dateOfBirth, int noOfContracts, double soldAmount,
+                  String city, String spz, double consumption, String ip)
+    {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.noOfContracts = noOfContracts;
+        this.soldAmount = soldAmount;
+        this.city = city;
+        this.spz = spz;
+        this.consumption = consumption;
+        this.ip = ip;
+    }
+
+    public String getName() { return name; }
+    public LocalDate getDateOfBirth() { return dateOfBirth; }
+    public int getNoOfContracts() { return noOfContracts; }
+    public double getSoldAmount() { return soldAmount; }
+    public String getCity() { return city; }
+    public String getSPZ() { return spz; }
+    public double getConsumption() { return consumption; }
+    public String getIP() { return ip; }
+
+    @Override public String toString()
+    {
+        String smlouvaStr;
+
+        if (this.getNoOfContracts() == 1) {
+            smlouvaStr = " smlouva";
+        } else if (this.getNoOfContracts() == 2) {
+            smlouvaStr = " smlouvy";
+        } else {
+            smlouvaStr = " smluv";
+        }
+
+        return (String.format(this.getName() + " dodal celkem " + this.getSoldAmount() + " tun zboží, navázáno na " +
+                        this.getNoOfContracts() + smlouvaStr + " (průměrně " + "%.0f" + " kg/smlouva).",
+                (1000*this.getSoldAmount()/this.getNoOfContracts()))
+        );
+    }
 }
